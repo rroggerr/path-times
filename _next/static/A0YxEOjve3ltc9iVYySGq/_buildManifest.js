47b4c5b1af009804beb56a45dbcd03ca61d958e5
rroggerr <rroggerr@users.noreply.github.com>
2022-12-19 22:17:19 +0000

    deploy: fd94cac074a7ca447c228887a16c5740f42329db

---
+++ _next/static/A0YxEOjve3ltc9iVYySGq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e6d4326438ef3630.css","static/chunks/pages/index-02218365686781c4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
