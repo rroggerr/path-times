e8cc90b84406e3bb382995bf2c88b7ac062d44f7
Roger Geng <geng.roger@gmail.com>
2022-12-20 13:58:23 -0500

    Test wakelock

---
+++ hooks/useConstantWakeLock.ts
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useWakeLock } from "react-screen-wake-lock";
+
+export const useConstantWakeLock = () => {
+  const { isSupported, request, release } = useWakeLock({});
+
+  useEffect(() => {
+    if (isSupported) {
+      request();
+    }
+
+    return () => {
+      release();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,23 @@ import Head from "next/head";
 import { Column } from "../components/Column";
 import styles from "../styles/Home.module.css";
 import { Direction, Station } from "../types/Train";
-import { useGetTimes } from "../utils/useGetTimes";
+import { useGetTimes } from "../hooks/useGetTimes";
+import { useConstantWakeLock } from "../hooks/useConstantWakeLock";
 
 export default function Home() {
   const station = Station.GROVE_STREET;
   const data = useGetTimes({ station, dir: Direction.ALL, len: 3 });
+  useConstantWakeLock();
 
   return (
     <>
       <Head>
         <title>Path Times</title>
         <meta name="description" content="Get the latest PATH train times" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
+        />
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
