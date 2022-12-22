1c9544162b076ffd13fbb0a02471c462b02e1a0f
Roger Geng <geng.roger@gmail.com>
2022-12-22 13:48:43 -0500

    status vars

--- pages/api/status.ts
+++ pages/api/status.ts
@@ -1,10 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const sha = process.env.VERCEL_GIT_COMMIT_SHA ?? 'localdev';
-
 export default async function handler(
   _: NextApiRequest,
-  res: NextApiResponse<string>
+  res: NextApiResponse<{ env: string; sha: string; git: string }>
 ) {
-  res.status(200).send(sha);
+  res.status(200).json({
+    env: process.env.VERCEL_ENV ?? 'local',
+    sha: process.env.VERCEL_GIT_COMMIT_SHA ?? 'localdev',
+    git: process.env.VERCEL_GIT_COMMIT_MESSAGE ?? 'localdev',
+  });
 }
