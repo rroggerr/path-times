21f2342f34b609b819b82d09badf575defc6b3df
Roger Geng <geng.roger@gmail.com>
2022-12-22 13:41:49 -0500

    enable relocate override

--- hooks/useStation.ts
+++ hooks/useStation.ts
@@ -1,11 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getNearestStation } from '../components/getStation';
 import { StationInfo } from '../types/Station';
 import { FALLBACK_STATION } from '../utils/StationInfo';
 
 export const useStation = () => {
+  const isModified = useRef(false);
   const [isLocating, setIsLocating] = useState(false);
-  const [station, setStation] = useState<StationInfo>(FALLBACK_STATION);
+  const [station, _setStation] = useState<StationInfo>(FALLBACK_STATION);
+
+  const setStation = (s: StationInfo) => {
+    _setStation(s);
+    isModified.current = true;
+  };
 
   useEffect(() => {
     relocate();
@@ -17,7 +23,9 @@ export const useStation = () => {
       ({ coords }) => {
         const nearest = getNearestStation(coords);
         setIsLocating(false);
-        setStation(nearest);
+        if (isModified.current === false) {
+          _setStation(nearest);
+        }
       },
       () => {
         setIsLocating(false);
