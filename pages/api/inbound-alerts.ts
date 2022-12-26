271991bec0561d845c93bc27046e776942331e4e
Roger Geng <geng.roger@gmail.com>
2022-12-26 10:32:36 -0500

    Use req.body for inbound-alerts

--- pages/api/inbound-alerts.ts
+++ pages/api/inbound-alerts.ts
@@ -3,33 +3,13 @@ import { readCache, writeCache } from '../../utils/apiCacheUtil';
 
 const ALERT_KEY = 'alerts';
 
-const fetchEmail = async () => {
-  const key = process.env.SENDINBLUE_API;
-  const resp = await fetch('https://api.sendinblue.com/v3/inbound/events', {
-    headers: { 'api-key': key ?? '' },
-  });
-  const json = await resp.json();
-  const emailId = json.events?.[0]?.uuid;
-
-  if (emailId) {
-    const emailresp = await fetch(
-      `https://api.sendinblue.com/v3/inbound/events/${emailId}`,
-      { headers: { 'api-key': key ?? '' } }
-    );
-    const emailJson = await emailresp.json();
-    return emailJson?.subject;
-  }
-  return null;
-};
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<string>
 ) {
   if (req.method === 'POST') {
-    const subj = await fetchEmail();
     console.log(JSON.stringify(req.body));
-    writeCache(ALERT_KEY, subj);
+    writeCache(ALERT_KEY, JSON.stringify(req.body));
     res.status(200).send('Message recieved');
   } else if (req.method === 'GET') {
     const data = await readCache<Object>(ALERT_KEY, 1000000);
