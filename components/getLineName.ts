c666be9150f2f8403e4cb7cc1094d925ed4eb71d
Roger Geng <geng.roger@gmail.com>
2023-01-02 23:41:34 -0500

    33rd st line naming and fallback

--- components/getLineName.ts
+++ components/getLineName.ts
@@ -2,12 +2,12 @@ const LINE_MAP: Record<string, string> = {
   'Journal Square via Hoboken': 'JS/HB',
   'Journal Square': 'JSQ',
   '33rd Street via Hoboken': '33/HB',
-  '33rd Street': '33',
+  '33rd Street': '33rd',
   Newark: 'NWK',
   Hoboken: 'HOB',
   'World Trade Center': 'WTC',
 };
 
 export const getShortLineName = (lineName: string): string => {
-  return LINE_MAP[lineName] ?? lineName;
+  return LINE_MAP[lineName] ?? lineName.substring(0, 3).toUpperCase();
 };
